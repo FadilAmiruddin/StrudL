0cfc62417890f1a103c74d28905fac93c76d4718
Fadil <fadilamiruddin@Fadils-MacBook-Air-2.local>
2024-06-27 11:18:52 +0200

    added google maps support for androidand added instructions to readme with how to start

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [showMap, setShowMap] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {showMap ? (
+        <MapView
+          style={styles.map}
+          
+          initialRegion={{
+            latitude: 48.2082,
+            longitude: 16.3738,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{ latitude: 48.2082, longitude: 16.3738 }}
+            title="Vienna"
+            description="Vienna, Austria"
+          />
+        </MapView>
+      ) : (
+        <>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Button title="Show Map" onPress={() => setShowMap(true)} />
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +42,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
---
+++ readme.md
@@ -0,0 +1,16 @@
+In order to start the app use the following command.
+
+npx expo start
+
+
+1. on mac you can download an ios simulater and use that to test code.
+2. windows can either use web or a android simulater or scan app on android.
+3. react should work the same on all platforms due to js logic and shit.
+
+
+Command to install react-native maps
+npm install react-native-maps --save
+
+Install cocopads (for mac/linux)
+gem install drb -v 2.0.6
+Sudo gem install cocoapods
